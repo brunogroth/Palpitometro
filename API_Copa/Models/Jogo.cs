686c136c6a0cfbf6dfc04ee148f1f5feecf858ff
brunogroth <brunom.groth@gmail.com>
2022-11-24 23:32:58 -0300

    feat: placar na model, cadastro jogo'

--- API_Copa/Models/Jogo.cs
+++ API_Copa/Models/Jogo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API_Copa.Models
 {
@@ -6,10 +7,11 @@ namespace API_Copa.Models
     {
         public Jogo()
         {
-            SelecaoA = new Selecao();
-            SelecaoB = new Selecao();
+            // SelecaoA = new Selecao();
+            // SelecaoB = new Selecao();
             CriadoEm = DateTime.Now;
         }
+        [Key]
         public int Id { get; set; }
         public Selecao SelecaoA { get; set; }
         public Selecao SelecaoB { get; set; }
